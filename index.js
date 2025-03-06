92e5c1e53c60eddd3c65a9890ab20d66162385b4
xaimohchau@gmail.com <xaimohchau@gmail.com>
2025-03-06 23:35:40 +0300

    Load saved tasks when the page loads

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
+document.addEventListener("DOMContentLoaded", loadTasks);
+
+
 const inputField = document.getElementById("todoInput");
 const todoList = document.getElementById("todolist");
 const deleteAllBtn = document.getElementById("deleteButton");
@@ -18,6 +21,7 @@ function addTask() {
 
     taskContent.onclick = function () {
         taskContent.classList.toggle("completed");
+        saveTasks();
     };
 
     const deleteBtn = document.createElement("button");
@@ -25,6 +29,7 @@ function addTask() {
     deleteBtn.classList.add("delete-btn")
     deleteBtn.onclick = function () {
         todoList.removeChild(taskItem);
+        saveTasks();
     };
 
     taskItem.appendChild(taskContent);
@@ -32,9 +37,58 @@ function addTask() {
     todoList.appendChild(taskItem);
 
     inputField.value = "";
+    saveTasks();
+}
+
+function saveTasks() {
+    const tasks = [];
+    document.querySelectorAll(".task-item p").forEach(task => {
+        tasks.push({
+            text: task.textContent,
+            completed: task.classList.contains("completed"),
+        });
+    });
+    localStorage.setItem("tasks", JSON.stringify(tasks));
+}
+
+//load items from local storage whem the page loads.
+function loadTasks() {
+    const savedTasks = JSON.parse(localStorage.getItem("tasks")) || [];
+
+    savedTasks.forEach(task => {
+        const taskItem = document.createElement("div");
+        taskItem.classList.add("task-item");
+
+        const taskContent = document.createElement("p");
+        taskContent.textContent = task.text;
+
+        if (task.completed) {
+            taskContent.classList.add("completed");
+        }
+
+        taskContent.onclick = function () {
+            taskContent.classList.toggle("completed");
+            saveTasks();
+        };
+
+        const deleteBtn = document.createElement("button");
+        deleteBtn.textContent = "x";
+        deleteBtn.classList.add("delete-btn");
+        deleteBtn.onclick = function () {
+            todoList.removeChild(taskItem);
+            saveTasks();
+        };
+
+        taskItem.appendChild(taskContent);
+        taskItem.appendChild(deleteBtn);
+        todoList.appendChild(taskItem);
+
+    });
 }
 
 deleteAllBtn.onclick = function () {
-    todoList.innerHTML = ""
-}
+    todoList.innerHTML = "";
+    localStorage.removeItem("tasks");
+};
+
 
